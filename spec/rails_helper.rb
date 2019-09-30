b889f93ebc47ffa69cbe4b130e8e8e0925a1e4ba
Charles Beynon <eToThePiIPower@gmail.com>
2019-09-30 16:45:39 -0400

    Add & install Shoulda::Matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/factory_bot'
+require 'support/shoulda_matchers'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
